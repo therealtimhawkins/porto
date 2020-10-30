835ae1917ea9c29751e9b0580dcba63e5a5f0be1
therealtimhawkins <timothyhaydenhawkins@gmail.com>
2020-10-30 18:29:42 +0000

    adds search term

--- index.js
+++ index.js
@@ -6,16 +6,21 @@ const runBash = async (command) => {
   return await exec(command)
 }
 
+const run = async (searchTerm) => {
+  const stdout = await getPortUsers()
+  const parseStdout = parseUsers(stdout, searchTerm)
+}
+
 const getPortUsers = async (path) => {
   try {
-    const { stdout } = await runBash(`netstat -Watnlv | grep LISTEN | awk '{"ps -o comm= -p " $9 | getline procname; print cred "" $1 " | " $4 " | " $9  " | " procname;  }' | column -t -s " |"`)
-    return parseUsers(stdout)
+    const { stdout } = await runBash(`netstat -Watnlv | grep -E 'LISTEN' | awk '{"ps -o comm= -p " $9 | getline procname; print cred "" $1 " | " $4 " | " $9  " | " procname;  }' | column -t -s " |"`)
+    return stdout
   } catch (err) {
     console.log(err)
   }
 }
 
-const parseUsers = (users) => {
+const parseUsers = (users, searchTerm) => {
   const parsedUsers = []
   const lines = users.split('\n').filter((el) => {
     return el != ''
@@ -24,18 +29,21 @@ const parseUsers = (users) => {
     const splitLine = line.split(' ').filter((el) => {
       return el != ''
     })
-    console.log(splitLine)
-    parsedUsers.push({
-      proto: splitLine[0],
-      port: splitLine[1],
-      pid: splitLine[2],
-      procname: splitLine[3]
-    })
-    console.log(parsedUsers)
+
+    if (line.includes(searchTerm)) {
+      parsedUsers.push({
+        proto: splitLine[0],
+        port: splitLine[1],
+        pid: splitLine[2],
+        procname: splitLine[3]
+      })
+    }
   })
+  console.log(parsedUsers)
+  return parsedUsers
 }
 
-getPortUsers()
+run('mysql')
 
 
 // netstat -Watnlv | grep LISTEN | awk '{"ps -o comm= -p " $9 | getline procname; print cred "proto: " colclr $1 colred " | addr.port: " colclr $4 colred " | pid: " colclr $9 colred " | name: " colclr procname;  }' | column -t -s "|"
